20688c6a1942f8472acbb5462a0eb1935d6a8d34
Attiq <0703thuy@gmail.com>
2022-06-17 17:07:36 +0700

    Create React.js

---
+++ React.js
@@ -0,0 +1,11 @@
+
+//arrow function
+  // function logger(log){
+  //   console.log(log);
+  // }
+  // logger('Message...');
+
+const logger = log => {
+  console.log(log);
+}
+logger("message...");
