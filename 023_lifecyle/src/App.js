b96cd3839e91536641930ef4aecd7e9b7f0db752
Attiq <0703thuy@gmail.com>
2022-06-20 14:59:35 +0700

    escape

--- 023_lifecyle/src/App.js
+++ 023_lifecyle/src/App.js
@@ -1,30 +1,34 @@
 import React, {Component} from 'react';
+import NoiDung from './NoiDung';
 //import logo from './logo.svg';
+
 import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
     this.state ={
-      trangthai:"khoitao"
+      trangthai:"khoitao",
+      trangthai2:"khoitao2",
     }
   }
 
   
   componentWillMount() {
-    console.log('componentWillMount')
+    console.log('componentWillMount da chay')
   }
   componentDidMount() {
     console.log('componentDidMount da chay roi');
   }
   
   shouldComponentUpdate(nextProps, nextState) {
-    console.log('componentDidMount da chay roi');  
+    console.log('shouldComponentUpdate da chay roi');  
+    return true;
   }
   componentWillUpdate(nextProps, nextState) {
-    console.log('componentDidMount da chay roi');
+    console.log('componentWillUpdate da chay roi');
   }
   componentDidUpdate(prevProps, prevState) {
-    console.log('componentDidMount da chay roi');
+    console.log('componentDidUpdate da chay roi');
   }
   
   capnhatState = () => {
@@ -32,14 +36,17 @@ class App extends Component {
       trangthai:"Trang thai duoc Update"
     });
   }
-
+    
   
   render() {
   
     console.log("in ra man hinh");
+    console.log(this.state.trangthai);
     return (
       <div className="App">
+        <NoiDung ten={this.state.trangthai2}/>
         <button onClick={() => this.capnhatState()}> Click de update</button>
+      
       </div>
     );
   }
---
+++ 023_lifecyle/src/NoiDung.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NoiDung extends Component {
+    render() {
+        return (
+            <div>
+                <h4>{this.props.ten}</h4>
+            </div>
+        );
+    }
+}
+
+export default NoiDung;
