00f59f07aba2117d36ccf92953eeec4fd02cd5c2
Attiq <0703thuy@gmail.com>
2022-06-20 13:38:41 +0700

    h

--- 013_component/src/component/Content/Content.js
+++ 013_component/src/component/Content/Content.js
@@ -24,7 +24,7 @@ class Content extends Component {
     renderForm = () => (
         <div className='row'>
             <div className="form-group">
-                <input defaultValue={this.props.tieude} type="text" name="ten" className="form-control" />
+                <input ref={(dulieunhap) => {this.trunggian = dulieunhap}} defaultValue={this.props.tieude} type="text" name="ten" className="form-control" />
             
             </div>
 
@@ -46,6 +46,7 @@ class Content extends Component {
     }
     saveClick =() => {
         this.setState({trangThai:0});
+        console.log(this.trunggian.value);
     }
 
     render() {
--- 023_lifecyle/src/App.js
+++ 023_lifecyle/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+//import logo from './logo.svg';
 import './App.css';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      trangthai:"khoitao"
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  componentWillMount() {
+    console.log('componentWillMount')
+  }
+  componentDidMount() {
+    console.log('componentDidMount da chay roi');
+  }
+  
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('componentDidMount da chay roi');  
+  }
+  componentWillUpdate(nextProps, nextState) {
+    console.log('componentDidMount da chay roi');
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidMount da chay roi');
+  }
+  
+  capnhatState = () => {
+    this.setState({
+      trangthai:"Trang thai duoc Update"
+    });
+  }
+
+  
+  render() {
+  
+    console.log("in ra man hinh");
+    return (
+      <div className="App">
+        <button onClick={() => this.capnhatState()}> Click de update</button>
+      </div>
+    );
+  }
 }
+
 
 export default App;
